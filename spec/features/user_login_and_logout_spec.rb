5f14220c25b1ca6eaad01a71213e20274081d81e
Carlos Del Real <carloshdelreal@gmail.com>
2020-02-07 23:19:12 -0500

    add user model unit testing and a feature test user logs in and out

---
+++ spec/features/user_login_and_logout_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+RSpec.feature 'User Logs in and logs out' do
+  scenario 'Test the log in and the log out', js: true do
+    user = FactoryBot.create(:user)
+    visit new_user_session_path
+    within('form') do
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+    end
+    click_button 'Log in'
+    expect(page).to have_content('Hello!')
+    click_on 'Sign out'
+    respond_to be_success
+  end
+end
