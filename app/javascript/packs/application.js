88d7e0d28c977a6afab0dfc5ac2714516cf44f3e
Carlos Del Real <carloshdelreal@gmail.com>
2020-02-09 10:56:24 -0500

    improve login styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min';
+import '../../assets/stylesheets/deviseLogin.scss';
+
+
 require('@rails/ujs').start();
 require('turbolinks').start();
 require('@rails/activestorage').start();
